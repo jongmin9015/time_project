1603c53543adbb69ddbd2cbf18e50cadbad42220
JJong <comeno3434@naver.com>
2022-03-24 13:02:39 +0900

    조인매핑수정중

--- timeProject/src/main/java/com/tis/domain/BoardVO.java
+++ timeProject/src/main/java/com/tis/domain/BoardVO.java
@@ -1,13 +1,5 @@
 package com.tis.domain;
-//create table tbl_board(
-//	    bno     number not null,        --게시물 번호
-//	    title   varchar2(200) not null, --제목
-//	    content varchar2(4000),         --내용
-//	    writer  varchar2(50) not null,  --이름
-//	    regdate date default sysdate,   --작성일자
-//	    viewcnt number default 0,       --조회수
-//	    PRIMARY key(bno)                --게시물 번호 기본키설정
-//	);
+
 
 import java.util.Date;
 
